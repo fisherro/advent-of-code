ab385bb960da257a5ada8da4c06a2deebb70ad13
Robert Fisher <robertfi@usau-robertfi-m.attlocal.net>
2021-11-18 02:54:30 -0600

    More 2020-2.rkt improvements

--- 2020-2/2020-2.rkt
+++ 2020-2/2020-2.rkt
@@ -2,24 +2,55 @@
 
 (require srfi/26)
 
+;;; Examples of the lines we'll have to parse:
 ;1-3 a: abcde
 ;1-3 b: cdefg
 ;2-9 c: ccccccccc
 
+;; The regexp for parsing the lines of the input
 (define rx #rx"^([0-9]+)-([0-9]+) ([a-z]): (.+)$")
 
+;;; How best to do the transforms?
+
+;; Compose list-update
+;; This smells inefficient.
+(define transform1
+  (compose1 (cute list-update <> 2 (cute string-ref <> 0))
+            (cute list-update <> 1 string->number)
+            (cute list-update <> 0 string->number)))
+
+;; Quasiquotation
+;; It seems a shame to walk the list separately for each element.
+(define (transform2 lst)
+  `(,(string->number (first lst))
+    ,(string->number (second lst))
+    ,(string-ref (third lst) 0)
+    ,(fourth lst)))
+
+;; for/list
+;; This seems like it would be the more efficient option.
+(define (transform3 lst)
+  (for/list (((value i) (in-indexed lst)))
+    (case i
+      ((0 1) (string->number value))
+      ((2) (string-ref value 0))
+      (else value))))
+
+;; Use callback to validate each line of the input.
+;; Return the count of valid lines.
 (define (process callback)
   (with-input-from-file "input.txt"
-    (thunk (for/fold ((valid-count 0))
-                     ((line (in-lines)))
-             (let* ((matches (regexp-match rx line))
-                    (min (string->number (second matches)))
-                    (max (string->number (third matches)))
-                    (char (string-ref (fourth matches) 0))
-                    (pw (fifth matches)))
-               (if (callback min max char pw)
-                   (add1 valid-count)
-                   valid-count))))))
+    (thunk
+     (sequence-count
+      (cute apply callback <>)
+      (sequence-map
+       (compose1
+        ;transform1
+        ;transform2
+        transform3
+        rest
+        (cute regexp-match rx <>))
+       (in-lines))))))
 
 ;; Part 1: Expects 424
 (process
