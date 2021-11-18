5505a232ab8f8b0cfbdd890cf270e17e414ac0ff
Robert Fisher <robertfi@usau-robertfi-m.attlocal.net>
2021-11-18 02:03:57 -0600

    2020-2.rkt improvements

--- 2020-2/2020-2.rkt
+++ 2020-2/2020-2.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;;; TODO: Use cut/cute or something similar for predicate?
+(require srfi/26)
 
 ;1-3 a: abcde
 ;1-3 b: cdefg
@@ -8,37 +8,28 @@
 
 (define rx #rx"^([0-9]+)-([0-9]+) ([a-z]): (.+)$")
 
-(define line "1-3 a: abcde")
-
 (define (process callback)
   (with-input-from-file "input.txt"
-    (λ ()
-      (let loop ((line (read-line))
-                 (valid-count 0))
-        (if (eof-object? line)
-            (displayln valid-count)
-            (let* ((matches (regexp-match rx line))
-                   (min (string->number (second matches)))
-                   (max (string->number (third matches)))
-                   (char (string-ref (fourth matches) 0))
-                   (pw (fifth matches))
-                   (is-valid (callback min max char pw)))
-              (if is-valid
-                  (loop (read-line)
-                        (add1 valid-count))
-                  (loop (read-line)
-                        valid-count))))))))
+    (thunk (for/fold ((valid-count 0))
+                     ((line (in-lines)))
+             (let* ((matches (regexp-match rx line))
+                    (min (string->number (second matches)))
+                    (max (string->number (third matches)))
+                    (char (string-ref (fourth matches) 0))
+                    (pw (fifth matches)))
+               (if (callback min max char pw)
+                   (add1 valid-count)
+                   valid-count))))))
 
-;; part 1
+;; Part 1: Expects 424
 (process
  (λ (min max char pw)
-   (let ((count (sequence-count (λ (x)
-                                  (equal? x char))
+   (let ((count (sequence-count (cute equal? char <>)
                                 pw)))
      (and (>= count min)
           (<= count max)))))
 
-;; part 2
+;; Part 2: Expects 747
 (process
  (λ (pos1 pos2 char pw)
    (xor (equal? char (string-ref pw (sub1 pos1)))
