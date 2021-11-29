6bae7064b24c3e0601d81577ce0b449f545d1a74
Robert Fisher <robertfi@usau-robertfi-m.attlocal.net>
2021-11-29 13:42:45 -0600

    2020-14 part 2

--- 2020/2020-14/2020-14.rkt
+++ 2020/2020-14/2020-14.rkt
@@ -93,7 +93,44 @@ mem[8] = 0
       (else v)))
   (vector-map helper value mask))
 #;(mask-it (parse-number "11")
-         (parse-mask "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"))
+           (parse-mask "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"))
+
+(define (mask-it2 value mask)
+  (define (update-results results index bit)
+    #;(printf "update-results ~s ~s ~s\n" results index bit)
+    (cond ((number? bit)
+           (map (cute cons bit <>)
+                results))
+          (else
+           (append
+            (map (cute cons 1 <>)
+                 results)
+            (map (cute cons 0 <>)
+                 results)))))
+  (define value-bitvector (parse-number value))
+  (aand (for/fold ((results (list '())))
+                  ((index (in-range 0 36)))
+          (define value-bit (vector-ref value-bitvector index))
+          (define mask-bit (vector-ref mask index))
+          (case mask-bit
+            ((0)
+             (update-results results index value-bit))
+            ((1)
+             (update-results results index 1))
+            (else
+             (update-results results index #f))))
+        (map reverse it)
+        (map list->vector it)
+        (map bitvector->number it)))
+
+(define mask-it2-test
+  (sort
+   (mask-it2
+    "42"
+    (parse-mask "000000000000000000000000000000X1001X"))
+   <))
+
+(expect 'mask-it2-test '(26 27 58 59))
 
 (define (part1 file)
   (with-input-from-file file
@@ -117,6 +154,33 @@ mem[8] = 0
                          bitvector->number
                          (in-hash-values mem))))))
 
+(define (part2 file)
+  (with-input-from-file file
+    (thunk
+     (define mem (make-hash))
+     (define mask (make-vector 36 #f))
+     (for ((line (in-lines)))
+       #;(printf "Line: ~s\n" line)
+       (acond ((string-prefix? line "mask = ")
+               (set! mask (parse-mask (substring line 7)))
+               #;(printf "Mask: ~s\n" mask))
+              ((regexp-match #rx"mem\\[([0-9]+)\\] = ([0-9]+)" line)
+               (define arg (parse-number (third it)))
+               (define addrs (mask-it2 (second it)
+                                       mask))
+               (for-each (cute hash-set! mem <> arg)
+                         addrs))
+              (else
+               (printf "Error!\n"))))
+     #;(writeln mem)
+     (sequence-fold + 0 (sequence-map
+                         bitvector->number
+                         (in-hash-values mem))))))
+
 (expect '(part1 "test.txt") 165)
 
 (expect '(part1 "input.txt") 7477696999511)
+
+(expect '(part2 "test2.txt") 208)
+
+(expect '(part2 "input.txt") 3687727854171)
